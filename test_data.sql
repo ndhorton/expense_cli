84a58abbda43d464fdc2c5092d1ccd8932e2bf7e
Nicholas Horton <nicholashorton7@protonmail.com>
2026-04-26 16:41:56 +0100

    Finish project

---
+++ test_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO expenses (created_on, amount, memo)
+VALUES
+('2016-04-07', 14.56, 'Pencils'),
+('2016-04-07', 3.29, 'Coffee'),
+('2016-04-07', 49.99, 'Text Editor'),
+('2016-04-07', 3.59, 'More Coffee');
